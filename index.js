cb6905ac539806f568b7775ac437ceb23e787783
vj_akash <48998005+vjakash@users.noreply.github.com>
2020-07-16 02:58:16 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hey Socket.io</h1>');
+});
+rooms = {};
+clients = {};
+io.on('connection', (socket) => {
+    console.log(`${socket.id} connected to socket`);
+    socket.on('create', (data) => {
+        console.log(`${data.userName} user joined Room-${data.roomName}`);
+        if (rooms[data.roomName] === undefined) {
+            rooms[data.roomName] = { users: [data.userName] };
+        } else {
+            rooms[data.roomName].users.push(data.userName);
+        }
+        clients[socket.id] = { roomName: data.roomName, userName: data.userName };
+        socket.join(data.roomName);
+        let users = rooms[clients[socket.id].roomName].users;
+        io.sockets.in(clients[socket.id].roomName).emit('joined', users);
+    })
+    socket.on('sendMessage', (message) => {
+        socket.to(clients[socket.id].roomName).emit('receiveMessage', message);
+    })
+    socket.on('disconnect', () => {
+        console.log(`${clients[socket.id].userName} user disconnected`);
+        // console.log(rooms[clients[socket.id]["roomName"]]["users"]);
+        let users = rooms[clients[socket.id].roomName]["users"];
+        users.splice(users.indexOf(clients[socket.id].userName, 1));
+        io.sockets.in(clients[socket.id].roomName).emit('joined', users);
+        clients[socket.id] = undefined;
+    });
+    socket.on('leave', () => {
+        socket.disconnect();
+    })
+});
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
