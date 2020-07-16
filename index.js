310559b83626c08c2532b158d09e3076a2661067
vj_akash <48998005+vjakash@users.noreply.github.com>
2020-07-16 14:53:36 +0530

    .

--- index.js
+++ index.js
@@ -26,10 +26,11 @@ io.on('connection', (socket) => {
     })
     socket.on('disconnect', () => {
         if (clients[socket.id] !== undefined) {
+            socket.leave(clients[socket.id].roomName);
             console.log(`user -${clients[socket.id].userName}  disconnected from room-${clients[socket.id].roomName}`);
             // console.log(rooms[clients[socket.id]["roomName"]]["users"]);
             let users = rooms[clients[socket.id].roomName]["users"];
-            users.splice(users.indexOf(clients[socket.id].userName, 1));
+            users.splice(users.indexOf(clients[socket.id].userName), 1);
             io.sockets.in(clients[socket.id].roomName).emit('joined', users);
             clients[socket.id] = undefined;
         }
